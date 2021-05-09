cb4e4b22899020ffd71ebe605e7447600d30ec92
Marie Roald <roald.marie@gmail.com>
2021-05-09 17:09:48 +0200

    Initial commit

---
+++ enrobot.py
@@ -0,0 +1,51 @@
+from p5 import *
+
+
+def setup():
+    width = 1000
+    height = 1000
+    size(width, height)
+    rect_mode("CENTER")
+    pass
+
+
+def draw():
+    color_mode('HSB', 360, 100, 100)
+    background( 22, 23, 95)
+
+    translate(width/2, height/2)
+
+    no_stroke()
+    fill(163, 44, 55)
+    rect(0, 0, 300, 400)
+    fill(163, 44, 50)
+    rect(0, -200-50, 200, 100)
+
+    fill(22, 61, 85)
+    for i in range(5):
+        rect( 80, 200 + 5 + 10*i, 50, 9)
+        rect(-80, 200 + 5 + 10*i, 50, 9)
+    fill(22, 61, 100)
+    rect( 80, 200 + 5 + 50, 80, 9)
+    rect(-80, 200 + 5 + 50, 80, 9)
+
+    fill(22, 61, 100)
+    stroke(22, 61, 85)
+    stroke_weight(2)
+    rect(-100, -150, 20, 30)
+    rect(-100, -115, 20, 20)
+
+    no_stroke()
+    fill(22, 61, 85)
+    for i in range(10):
+        rect(-155 - 10*i, -120, 9, 80*(1.02**i))
+        rect(155 + 10*i, -120, 9, 80*(1.02**i))
+
+    fill(186, 40, 25)
+    rect(-55, -250, 50, 50 )
+    rect( 55, -250, 50, 50 )
+    
+    no_loop()
+
+
+run()
---
+++ sirkler.py
@@ -0,0 +1,51 @@
+
+from p5 import *
+
+def setup():
+    width = 2000
+    height = 1000
+    size(width, height)
+    background(33,46,64)
+
+
+def draw_eyeball(x, y, r):
+    with push_matrix():
+        translate(x, y)
+
+        fill(255,255,255)
+        stroke_weight(0)
+        circle((0, 0), r)
+
+        fill(
+            242*random_gaussian(mean=1, std_dev=0.1),
+            190*random_gaussian(mean=1, std_dev=0.1),
+            92*random_gaussian(mean=1, std_dev=0.1),
+            )
+        stroke_weight(r/20)
+        stroke(33, 46, 64)
+        translate(random_uniform(low=-r*0.1, high=r*0.1), 
+                  random_uniform(low=-r*0.1, high=r*0.1))
+        circle((0,0),
+                random_uniform(0.3, 0.7)*r)
+
+        
+        translate(random_uniform(low=-r*0.05, high=r*0.05), 
+                  random_uniform(low=-r*0.05, high=r*0.05))
+
+        fill(33, 46, 64)
+        stroke_weight(0)
+        circle((0,0),random_uniform(0.1, 0.2)*r)
+
+def draw():
+    background(33,46,64)
+
+    w = 120
+    for i in range(0, width, w):
+        for j in range(0, height, w):
+            xpos = i + random_uniform(-10, 10)
+            ypos = j + random_uniform(-10, 10)
+            draw_eyeball(xpos, ypos, random_uniform(0.8, 1.1)*90)
+
+    no_loop()
+
+run()
